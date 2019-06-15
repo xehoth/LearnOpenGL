11268f89a93db78edf4ea659625418dd0aa9339e
xehoth <824547322@qq.com>
2019-06-15 14:29:30 +0800

    Intro-Triangle

---
+++ src/Intro-Triangle/README.md
@@ -0,0 +1,4 @@
+# Intro-Triangle
+- Remember to run `Intro-Triangle` with `vertex.glsl` and `fragment.glsl`.
+- Press `F` to change PolygonMode to `GL_FILL`.
+- Press `L` to change PolygonMode to `GL_LINE`.
--- src/Intro-Triangle/main.cpp
+++ src/Intro-Triangle/main.cpp
@@ -1,25 +1,80 @@
 #include <glad/glad.h>
 #include <Window.h>
+#include <Shader.h>
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
+
+void processInput(const LearnOpenGL::Window &);
 
 int main() {
     LearnOpenGL::Window::defaultInit();
     LearnOpenGL::Window window(WINDOW_WIDTH, WINDOW_HEIGHT, "Intro-Triangle");
     LearnOpenGL::Window::loadOpenGL();
     window.setFramebufferSizeCallback();
+    LearnOpenGL::Shader shader("vertex.glsl", "fragment.glsl");
+
+    const GLfloat vertices[]{
+        0.5f,  0.5f,  0.0f,  // top right
+        0.5f,  -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f, 0.5f,  0.0f   // top left
+    };
+
+    GLuint indices[]{
+        0, 1, 3,  // first
+        1, 2, 3   // second
+    };
+
+    GLuint vao, vbo, ebo;
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
 
     while (!window.shouldClose()) {
-        if (window.isPressed(GLFW_KEY_ESCAPE)) {
-            window.setWindowShouldClose();
-        }
+        processInput(window);
 
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        shader.use();
+
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         window.updateFrame();
     }
 
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+
     glfwTerminate();
+}
+
+void processInput(const LearnOpenGL::Window &window) {
+    if (window.isPressed(GLFW_KEY_L)) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+    if (window.isPressed(GLFW_KEY_F)) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+    if (window.isPressed(GLFW_KEY_ESCAPE)) {
+        window.setWindowShouldClose();
+    }
 }
